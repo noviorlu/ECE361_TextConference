ac0e136ccad8207ba833510a2d67a17c8228af2f
Jack On ECF <jackchen.chen@mail.utoronto.ca>
2021-11-15 15:04:35 -0500

    debug1

---
+++ build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/bin/client.c.o"
+  "CMakeFiles/client.dir/src/sessionDB.c.o"
+  "CMakeFiles/client.dir/src/usrDB.c.o"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/u/a/cheny811/Desktop/ECE361_TextConference/bin/server.c" "/u/a/cheny811/Desktop/ECE361_TextConference/build/CMakeFiles/server.dir/bin/server.c.o"
+  "/u/a/cheny811/Desktop/ECE361_TextConference/src/sessionDB.c" "/u/a/cheny811/Desktop/ECE361_TextConference/build/CMakeFiles/server.dir/src/sessionDB.c.o"
+  "/u/a/cheny811/Desktop/ECE361_TextConference/src/usrDB.c" "/u/a/cheny811/Desktop/ECE361_TextConference/build/CMakeFiles/server.dir/src/usrDB.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
